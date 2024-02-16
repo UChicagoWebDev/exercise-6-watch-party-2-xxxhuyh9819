cfe70c6effae236588c90dda9c552d51929ee57f
yunhao <xxxhuyh9819@gmail.com>
2024-02-16 00:35:25 -0600

    Implemented sign up API on the backend

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import sqlite3
 import string
 import random
 from datetime import datetime
-from flask import Flask, g
+from flask import *
 from functools import wraps
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -16,11 +18,13 @@ def get_db():
         setattr(g, '_database', db)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 def query_db(query, args=(), one=False):
     db = get_db()
@@ -29,20 +33,22 @@ def query_db(query, args=(), one=False):
     db.commit()
     cursor.close()
     if rows:
-        if one: 
+        if one:
             return rows[0]
         return rows
     return None
+
 
 def new_user():
     name = "Unnamed User #" + ''.join(random.choices(string.digits, k=6))
     password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
     api_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
-    u = query_db('insert into users (name, password, api_key) ' + 
-        'values (?, ?, ?) returning id, name, password, api_key',
-        (name, password, api_key),
-        one=True)
+    u = query_db('insert into users (name, password, api_key) ' +
+                 'values (?, ?, ?) returning id, name, password, api_key',
+                 (name, password, api_key),
+                 one=True)
     return u
+
 
 # TODO: If your app sends users to any other routes, include them here.
 #       (This should not be necessary).
@@ -54,23 +60,26 @@ def new_user():
 def index(chat_id=None):
     return app.send_static_file('index.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return app.send_static_file('404.html'), 404
-
 
 
 # -------------------------------- API ROUTES ----------------------------------
 
 # TODO: Create the API
 
-# @app.route('/api/signup')
-# def login():
-#   ...
+'''
+API for sign up
+'''
+@app.route('/api/signup', methods=['POST'])
+def signUp():
+    newUser = new_user()
+    return jsonify({
+        "user_id": newUser["id"],
+        "user_name": newUser["name"],
+        "password": newUser["password"],
+        "api_key": newUser["api_key"],
+    }), 200
 
-
-# @app.route('/api/login')
-# def login():
-#   ... 
-
-# ... etc
